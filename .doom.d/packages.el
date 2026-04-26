aa926992ad0795286de6b67dc73b34a4e94b9139
BaptisteLoquette <Baptisteloquette.entr@gmail.com>
2026-04-26 18:48:02 +0200

    feat: add packages.el and end-to-end verification suite

---
+++ .doom.d/packages.el
@@ -0,0 +1,24 @@
+;; -*- no-byte-compile: t; -*-
+;; Extra packages beyond Doom modules
+
+;; PKM
+(package! org-roam-ui)
+(package! org-roam-bibtex)
+(package! org-ql)
+
+;; Literature
+(package! org-ref)
+(package! elfeed)
+(package! org-noter)
+(package! pdf-tools)
+
+;; Diagrams
+(package! graphviz-dot-mode)
+(package! ob-mermaid)
+
+;; Animation
+(package! org-inline-anim)
+
+;; AI Assistants
+(package! gptel)
+(package! org-ai)
