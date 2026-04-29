fd85ad9954fe58e347feccf4023c167f04647d75
BaptisteLoquette <Baptisteloquette.entr@gmail.com>
2026-04-29 14:06:07 +0200

    feat: integrate org-ai-search into Doom + README (Tasks 12-13)

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -16,9 +16,34 @@
 (package! graphviz-dot-mode)
 (package! ob-mermaid)
 
-;; Animation
+;; ASCII / Unicode Drawing
+(package! uniline)
+
+;; Embedded SVG Drawing
+(package! edraw-org
+  :recipe (:host github :repo "misohena/el-easydraw" :files ("*.el" "*.svg")))
+(package! sketch-mode)
+
+;; Animation & Video
 (package! org-inline-anim)
+(package! svg-lib)
+
+;; Visualization & Plots
+;; ob-gnuplot is built into modern org-mode (obsoleted on MELPA)
+;; (package! ob-gnuplot)
+;; ob-ipython is superseded by built-in ob-jupyter
+;; (package! ob-ipython)
 
 ;; AI Assistants
 (package! gptel)
 (package! org-ai)
+(package! aider
+  :recipe (:host github :repo "tninja/aider.el" :files ("*.el" "snippets")))
+(package! ellama)
+(package! copilot
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el")))
+
+;; Org-AI-Search (local)
+(package! org-ai-search
+  :recipe (:local-repo "modules/org-ai-search"
+           :files (:defaults "python" "python/**/*")))
